065b6ac9463d03307eb9f7d5b8580e87d730cf00
Israel Galadima <izzygaladima@gmail.com>
2023-04-12 18:54:16 +0100

    Add caching

--- index.js
+++ index.js
@@ -1,127 +1,111 @@
-require("dotenv").config();
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
+import { config } from "dotenv";
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+import { MongoClient } from "mongodb";
 
+config();
 const app = express();
 
 app.use(
-	cors({
-		origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
-	})
+  cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  })
 );
 
 app.use(express.json());
 
+let mongoClient = await new MongoClient(process.env.MONGO_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}).connect()
+
+let transactions = mongoClient.db("Waffles").collection("Transactions")
+
 const apiUrl = "https://api.flutterwave.com/v3/transactions";
 const apiKey = process.env.FLUTIL_API_KEY;
 
-app.get("/api/transaction/:id", (req, res) => {
-	const id = req.params.id;
-	axios
-		.get(`${apiUrl}/id`, {
-			headers: {
-				Authorization: `Bearer ${apiKey}`,
-			},
-		})
-		.then((response) => response.data)
-		.then((data) => {
-			console.log(data);
-			res.send(data);
-		})
-		.catch((error) => {
-			console.log(error);
-			res.json({ error: "transaction not found" });
-		});
-});
-
-app.get("/api/transactions/:startDate", async (req, res) => {
-	const startDate = req.params.startDate;
-	let pageNumber = 1;
-	const allTransactions = [];
-	while (pageNumber) {
-		console.log(pageNumber);
-		await axios
-			.get(`${apiUrl}?page=${pageNumber.toString()}&from=${startDate}`, {
-				headers: {
-					Authorization: `Bearer ${apiKey}`,
-				},
-			})
-			.then((response) => response.data)
-			.then((data) => {
-				console.log(data.data);
-				allTransactions.push(...data.data);
-				if (pageNumber < data.meta.page_info.total_pages) {
-					pageNumber++;
-				} else {
-					pageNumber = false;
-					console.log(allTransactions[0]);
-					res.send(allTransactions);
-				}
-				// res.send(data);
-			})
-			.catch((error) => {
-				console.log(error);
-				res.json({ error: "transaction not found" });
-			});
-	}
-});
-
 app.get("/api/transactions", async (req, res) => {
-	let pageNumber = 1;
-	let total_pages = 0;
-	const allTransactions = [];
-	while (pageNumber) {
-		// await axios
-		//   .get(
-		//     `${apiUrl}?from=2023-04-11&status=successful&page=${pageNumber.toString()}`,
-		//     {
-		//       headers: {
-		//         Authorization: `Bearer ${apiKey}`,
-		//       },
-		//     }
-		//   )
-		//   .then((response) => response.data)
-		//   .then((data) => {
+  let pageNumber = 1;
+  let total_pages = 0;
+  const allTransactions = [];
+  while (pageNumber) {
+    await fetch(
+      `${apiUrl}?from=2023-04-11&status=successful&page=${pageNumber.toString()}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then(async (data) => {
+        let flutterwaveTransactionCount = data.meta.page_info.total
+        let cacheIsCurrent = await checkCache(flutterwaveTransactionCount)
+        if (cacheIsCurrent) {
+          let cachedTransactions = await readTransactions()
+          pageNumber = false;
+          res.send(cachedTransactions);
+        } else {
+          total_pages = Math.ceil(data.meta.page_info.total / 100);
 
-		console.log("starting fetch");
+          if (data.data.length > 10) {
+            console.log(data.data.length);
+            allTransactions.push(...data.data);
+            if (pageNumber === total_pages) {
 
-		await fetch(
-			`${apiUrl}?from=2023-04-11&status=successful&page=${pageNumber.toString()}`,
-			{
-				headers: {
-					Authorization: `Bearer ${apiKey}`,
-				},
-			}
-		)
-			.then((response) => response.json())
-			.then((data) => {
-				total_pages = Math.ceil(data.meta.page_info.total / 100);
-				console.log(data.data.length);
+              pageNumber = false;
+              await insertTransactions(allTransactions);
+              return res.send(allTransactions);
+            }
+            pageNumber++;
+          } else if (
+            allTransactions.length < data.meta.page_info.total &&
+            data.meta.page_info.total_pages == total_pages
+          ) {
+            console.log(data.data.length);
 
-				if (data.data.length > 10) {
-					allTransactions.push(...data.data);
-					if (pageNumber === total_pages) {
-						pageNumber = false;
-						return res.send(allTransactions);
-					}
-					pageNumber++;
-				} else if (
-					allTransactions.length < data.meta.page_info.total &&
-					data.meta.page_info.total_pages == total_pages
-				) {
-					allTransactions.push(...data.data);
-					pageNumber = false;
-					return res.send(allTransactions);
-				}
-			})
-			.catch((error) => {
-				console.log(error);
-				res.json({ error: "transaction not found" });
-			});
-	}
+            allTransactions.push(...data.data);
+            pageNumber = false;
+            await insertTransactions(allTransactions);
+            return res.send(allTransactions);
+          }
+        }
+      })
+      .catch(async (error) => {
+        console.log(error);
+        let cachedTransactions = await readTransactions()
+        pageNumber = false;
+        return res.send(cachedTransactions)
+      });
+  }
 });
 
 app.listen(4000, () => {
-	console.log("server is running");
+  console.log("server is running");
 });
+
+
+async function insertTransactions(data) {
+  for (let transaction of data) {
+    // make sure is unique
+    await transactions.insertOne({ ...transaction, "_id": transaction.id }, { upsert: true })
+  }
+}
+
+// need to add date field
+async function readTransactions() {
+  let cursor = await transactions.find({})
+  let allTransactions = await cursor.toArray()
+  return allTransactions
+}
+
+async function checkCache(count) {
+  let cursor = await transactions.find({})
+  let cacheCount = await cursor.count()
+  if (cacheCount === count) {
+    return true
+  } else {
+    return false
+  }
+}
