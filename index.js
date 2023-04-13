11027f29630fb5f9583057457da7e8d653b92165
Israel Galadima <izzygaladima@gmail.com>
2023-04-13 20:13:10 +0100

    Fix cors routes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://studentaffairs.cu.edu.ng/", "https://studentaffairs.cu.edu.ng/"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://studentaffairs.cu.edu.ng", "https://studentaffairs.cu.edu.ng"],
   })
 );
 
