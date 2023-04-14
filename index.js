db7454ee549fd4449834126a11d874e565444223
cyberbishop <alabialvin@gmail.com>
2023-04-14 12:39:29 +0100

    fix missing/reducing transactions

--- index.js
+++ index.js
@@ -8,105 +8,116 @@ config();
 const app = express();
 
 app.use(
-  cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://studentaffairs.cu.edu.ng", "https://studentaffairs.cu.edu.ng"],
-  })
+	cors({
+		origin: [
+			"http://localhost:5173",
+			"http://127.0.0.1:5173",
+			"http://studentaffairs.cu.edu.ng",
+			"https://studentaffairs.cu.edu.ng",
+		],
+	})
 );
 
 app.use(express.json());
 
 let mongoClient = await new MongoClient(process.env.MONGO_URI, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-}).connect()
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+}).connect();
 
-let transactions = mongoClient.db("Waffles").collection("Transactions")
+let transactions = mongoClient.db("Waffles").collection("Transactions");
 
 const apiUrl = "https://api.flutterwave.com/v3/transactions";
 const apiKey = process.env.FLUTIL_API_KEY;
 
 app.get("/api/transactions", async (req, res) => {
-  let cachedTransactions = await readTransactions()
-  res.send(cachedTransactions);
+	let cachedTransactions = await readTransactions();
+	console.log(cachedTransactions.length);
+	res.send(cachedTransactions);
 });
 
 app.get("/api/update", async (req, res) => {
-  let pageNumber = 1;
-  let total_pages = 0;
-  const allTransactions = [];
-  while (pageNumber) {
-    await fetch(
-      `${apiUrl}?from=2023-04-11&status=successful&page=${pageNumber.toString()}`,
-      {
-        headers: {
-          Authorization: `Bearer ${apiKey}`,
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then(async (data) => {
-        let flutterwaveTransactionCount = data.meta.page_info.total
+	let pageNumber = 1;
+	let total_pages = 0;
+	const allTransactions = [];
+	while (pageNumber) {
+		await fetch(
+			`${apiUrl}?from=2023-04-11&status=successful&page=${pageNumber.toString()}`,
+			{
+				headers: {
+					Authorization: `Bearer ${apiKey}`,
+				},
+			}
+		)
+			.then((response) => response.json())
+			.then(async (data) => {
+				let flutterwaveTransactionCount = data.meta.page_info.total;
 
-        let cacheIsCurrent = await checkCache(flutterwaveTransactionCount)
-        if (!cacheIsCurrent) {
-          total_pages = Math.ceil(data.meta.page_info.total / 10);
+				let cacheIsCurrent = await checkCache(flutterwaveTransactionCount);
+				if (!cacheIsCurrent) {
+					total_pages = Math.ceil(data.meta.page_info.total / 10);
+					console.log(total_pages);
 
-          if (data.data.length < 11) {
-            allTransactions.push(...data.data);
-            if (pageNumber === total_pages) {
-
-              pageNumber = false;
-              await insertTransactions(allTransactions);
-              return res.json({ "message": "Updated" });
-            }
-            pageNumber++;
-          } else if (
-            allTransactions.length < data.meta.page_info.total &&
-            data.meta.page_info.total_pages == total_pages
-          ) {
-            allTransactions.push(...data.data);
-            pageNumber = false;
-            await insertTransactions(allTransactions);
-            return res.json({ "message": "Updated" });
-          }
-        } else {
-          pageNumber = false;
-          res.json({ "message": "Up to date" })
-        }
-      })
-      .catch(async (error) => {
-        console.log(error);
-        let cachedTransactions = await readTransactions()
-        pageNumber = false;
-        return res.send(cachedTransactions)
-      });
-  }
+					if (data.data.length < 11 && total_pages !== 0) {
+						console.log(`fetching page ${pageNumber}`);
+						allTransactions.push(...data.data);
+						if (pageNumber === total_pages) {
+							pageNumber = false;
+							await insertTransactions(allTransactions);
+							return res.json({ message: "Updated" });
+						}
+						pageNumber++;
+					} else if (
+						allTransactions.length < data.meta.page_info.total &&
+						data.meta.page_info.total_pages == total_pages
+					) {
+						allTransactions.push(...data.data);
+						pageNumber = false;
+						await insertTransactions(allTransactions);
+						return res.json({ message: "Updated" });
+					}
+				} else {
+					pageNumber = false;
+					res.json({ message: "Up to date" });
+				}
+			})
+			.catch(async (error) => {
+				console.log(error);
+				let cachedTransactions = await readTransactions();
+				pageNumber = false;
+				return res.send(cachedTransactions);
+			});
+	}
 });
-
 
 app.listen(4000, () => {
-  console.log("server is running");
+	console.log("server is running");
 });
 
-
 async function insertTransactions(data) {
-    // make sure is unique
+	// make sure is unique
 
-  await transactions.updateOne({"_id": process.env.DOC_ID}, {$set: {data}}, {upsert: true})
+	await transactions.updateOne(
+		{ _id: process.env.DOC_ID },
+		{ $set: { data } },
+		{ upsert: true }
+	);
 }
 
 // need to add date field
 async function readTransactions() {
-  let doc = await transactions.findOne({"_id": process.env.DOC_ID})
-  return doc.data
+	let doc = await transactions.findOne({ _id: process.env.DOC_ID });
+	return doc.data;
 }
 
 async function checkCache(count) {
-  let doc = (await transactions.findOne({"_id": process.env.DOC_ID})) ?? { data: [] }
-  let cacheCount = doc.data.length
-  if (cacheCount === count) {
-    return true
-  } else {
-    return false
-  }
+	let doc = (await transactions.findOne({ _id: process.env.DOC_ID })) ?? {
+		data: [],
+	};
+	let cacheCount = doc.data.length;
+	if (cacheCount === count) {
+		return true;
+	} else {
+		return false;
+	}
 }
